80e8818492fe300f99b5adf0ca75fc90ede5f26a
Diego Lirio <diegolirio.dl@gmail.com>
2013-04-03 10:55:40 -0300

    Todo.txt

--- new/style.php
+++ new/style.php
@@ -2,5 +2,5 @@
 	<!-- Bootswatch: Amelia | Cerulean | Cosmo | Cyborg | Journal | Readable | Simplex | Slate | Spacelab | Spruce | Superhero | United -->
 	<!-- FortAwesome -->
 	<!-- Darkstrap -->
-	<link href="bootstrap/Readable/bootstrap.min.css" rel="stylesheet"> 
-	<link href="bootstrap/Readable/bootstrap.css" rel="stylesheet"> 
+	<link href="bootstrap/FortAwesome/bootstrap.min.css" rel="stylesheet"> 
+	<link href="bootstrap/FortAwesome/bootstrap.css" rel="stylesheet"> 
